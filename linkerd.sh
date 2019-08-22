01b1ebcd4ebe348b7636c60179e44353f5fc3bbd
nfyxhan <yangxiannfyxhan@163.com>
2019-08-22 17:27:05 +0800

    sync image

--- linkerd.sh
+++
@@ -1,6 +0,0 @@
-all="proxy proxy-init controller grafana web"
-for image in ${all} ; do
-    fullimage=registry.cn-beijing.aliyuncs.com/nfyxhan/linkerd-io_proxy-init:${image}
-    docker pull ${fullimage}
-    docker tag ${fullimage} gcr.io/linkerd-io/${image}:stable-2.0.0
-done
--- sync_image.sh
+++ sync_image.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash
 set -e
-
+registry=${registry:-"registry.cn-hangzhou.aliyuncs.com"}
+repo=${registry}/${repo:-"nfyxhan_gcr"}
 docker login --username=${username} --password=${password} ${registry}
 
 all_image=`cat image.list`
 
-for image in ${all_image} ; do
-    name=`echo ${image} | awk -F "@" '{ print $1}'| awk -F "/" '{ print $NF }'`
-    sync_image=${repo}:${prefix}${name}
-    docker pull ${image}
-    docker tag ${image} ${sync_image}
+for line in ${all_image} ; do
+    start=`echo ${line} | awk -F "@" '{ print $1 }'`
+    if [[ "${start}" == "" ]] ; then
+    	remote_repo=`echo "${line}" | awk -F "@" '{ print $2 }'`
+    	prefix=`echo "${line}" | awk -F "@" '{ print $3 }'`
+        continue
+	#break
+    fi
+    name=`echo ${line} | awk -F "@" '{ print $1}'| awk -F "/" '{ print $NF }'`
+    remote_image=${remote_repo}/${name}
+    sync_image=${repo}/${prefix}${name}
+    echo ${remote_image}
+    echo ${sync_image}
+    docker pull ${remote_image}
+    docker tag ${remote_image} ${sync_image}
     docker push ${sync_image}
 done
